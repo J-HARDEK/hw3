3d9fa87927fe08e42bb9e515062e29311ed73d93
John Hardek <john.hardek@kellogg.northwestern.edu>
2025-02-26 23:29:58 +0000

    Still can't get it to loop through entries

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,15 @@ class EntriesController < ApplicationController
   end
 
   def new
-    @entry = Entry.new
-    @entry["place_id"] = params["place_id"] 
+      @entry = Entry.new({ "place_id" => params["place_id"] })
   end
 
   def show
     @entry = Entry.find(params["id"])
+    @place = Place.find_by(@entry["place_id"]) # Ensuring we get the correct place
+    @entries = Entry.where("place_id" => @place["id"]) # Fetching entries for this place
   end
+  
 
   def create
     @entry = Entry.new
