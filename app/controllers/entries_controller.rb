2367cb72b0528f361a88126a0d4272ae87efa973
John Hardek <john.hardek@kellogg.northwestern.edu>
2025-02-26 14:14:47 +0000

    fixed some things with relating place_id back to entries. caused more problems. such is life.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,26 +1,25 @@
 class EntriesController < ApplicationController
   def index
-    @entries = Entry.all
+    @entry = Entry.all
   end
 
   def show
-    @entries = Place.find_by({ "name" => params["name"] })
-    @places = Entry.where({ "place_id" => @places["id"] })
+    @entry = Entry.find(params["id"]) # Find the specific entry by ID
+    @place = Place.find(@entry["place_id"]) # Find the related place
   end
 
   def new
+    @entry = Entry.new
+    @entry["place_id"] = params["place_id"]
   end
 
   def create
-    #   # start with a new Place
-    @entries = Entry.new
-    #   # assign user-entered form data to Post's columns
-    @entries["title"] = params["title"]
-    @entries["description"] = params["description"]
-
-    #   # save Post row
-    @entries.save
-    #   # redirect user
-    redirect_to "/entries"
+    @entry = Entry.new
+    @entry["title"] = params["title"]
+    @entry["description"] = params["description"]
+    @entry["occurred_on"] = params["occurred_on"]
+    @entry["place_id"] = params["place_id"]
+    @entry.save
+    redirect_to "/places/#{@entry["place_id"]}"
     end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,13 +10,9 @@ class PlacesController < ApplicationController
   end
 
   def create
-    #   # start with a new Place
     @places = Place.new
-    #   # assign user-entered form data to Post's columns
     @places["name"] = params["name"]
-    #   # save Post row
     @places.save
-    #   # redirect user
     redirect_to "/places"
     end
 
